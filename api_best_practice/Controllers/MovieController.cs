3a92c606c40f216d6d54081a6c3a28d9e4429a9e
gorkem acar <54164989+gor2em@users.noreply.github.com>
2024-09-17 13:37:39 +0300

    Update MovieController.cs

--- api_best_practice/Controllers/MovieController.cs
+++ api_best_practice/Controllers/MovieController.cs
@@ -11,8 +11,9 @@ namespace api_best_practice.Controllers
     public class MovieController : ControllerBase
     {
         private readonly HttpClient _httpClient;
-        private readonly string _apiKey = "5be2292958bad7179efadcebb8708b36"; // Buraya API anahtarınızı girin
-
+        // private readonly string _apiKey = "5be2292958bad7179efadcebb8708b36"; // Buraya API anahtarınızı girin
+        // your token.
+        private readonly _token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOGYyNTk4NDk4MmQyMTI0NWEwYjM0MDEzZmNlYTFiYSIsIm5iZiI6MTcyNjU2ODc1Ny4yNzM3ODUsInN1YiI6IjVlM2ZlNDc1MGMyNzEwMDAxMzdjZWYxZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yBJ7WX1Q3oPyuzJxEXSbPK_8JEfLvg9OimzrkmeRdOM';
         public MovieController(HttpClient httpClient)
         {
             _httpClient = httpClient;
@@ -21,9 +22,13 @@ namespace api_best_practice.Controllers
         [HttpGet]
         public async Task<IActionResult> GetPopularMovies([FromQuery] int page = 1)
         {
-            string url = $"https://api.themoviedb.org/3/movie?api_key={_apiKey}&language=en-US&page={page}";
+            // string url = $"https://api.themoviedb.org/3/movie?api_key={_apiKey}&language=en-US&page={page}";
+            string url = $"https://api.themoviedb.org/3/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200&page=${page}";
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+    
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", _token);
 
-            var response = await _httpClient.GetAsync(url);
+            var response = await _httpClient.GetAsync(request);
 
             if (!response.IsSuccessStatusCode)
             {
